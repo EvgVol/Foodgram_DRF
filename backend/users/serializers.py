d8b5c917aadd62b838bc34d9bc5c0b50e3d9fd48
EvgVol <volochek93@yandex.ru>
2023-02-16 14:44:27 +0300

    serializers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,36 +5,7 @@ from .models import User
 from .validators import validate_username, username_me
 
 
-class SingUpSerializer(serializers.Serializer):
-    """Сериализатор для регистрации."""
-
-    email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
-    )
-    username = serializers.CharField(
-        required=True,
-        validators=[validate_username, ]
-    )
-
-    def validate_username(self, value):
-        return username_me(value)
-
-
-class GetTokenSerializer(serializers.Serializer):
-    """Сериализатор для получения токена при регистрации."""
-
-    username = serializers.CharField(
-        required=True,
-        validators=(validate_username, )
-    )
-    confirmation_code = serializers.CharField(required=True)
-
-    def validate_username(self, value):
-        return username_me(value)
-
-
-class UsersSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     """Сериализатор для новых пользователей."""
 
     username = serializers.CharField(
@@ -49,3 +20,34 @@ class UsersSerializer(serializers.ModelSerializer):
         abstract = True
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', )
+
+# class SingUpSerializer(serializers.Serializer):
+#     """Сериализатор для регистрации."""
+
+#     email = serializers.EmailField(
+#         required=True,
+#         validators=[UniqueValidator(queryset=User.objects.all())]
+#     )
+#     username = serializers.CharField(
+#         required=True,
+#         validators=[validate_username, ]
+#     )
+
+#     def validate_username(self, value):
+#         return username_me(value)
+
+
+# class GetTokenSerializer(serializers.Serializer):
+#     """Сериализатор для получения токена при регистрации."""
+
+#     username = serializers.CharField(
+#         required=True,
+#         validators=(validate_username, )
+#     )
+#     confirmation_code = serializers.CharField(required=True)
+
+#     def validate_username(self, value):
+#         return username_me(value)
+
+
+
