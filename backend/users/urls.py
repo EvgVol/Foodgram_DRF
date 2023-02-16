470a077b9c370128ad71a3d70866a41145c4345b
EvgVol <volochek93@yandex.ru>
2023-02-16 12:22:21 +0300

    update setting

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,17 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from .views import UsersViewSet
+
 app_name = 'users'
 
+
+router_v1 = routers.DefaultRouter()
+
+router_v1.register(r'users', UsersViewSet, basename='users')
+
+urlpatterns = [
+    path('', include(router_v1.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+]
