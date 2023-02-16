a414a5942b7d1475949fe76019557ca3ee16b383
EvgVol <volochek93@yandex.ru>
2023-02-16 14:01:32 +0300

    add test_user_registration.py

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -6,18 +6,6 @@ from .common import auth_client, create_users_api
 
 class TestUserAPI:
 
-    @pytest.mark.django_db(transaction=True)
-    def test_auth_urls(self, client):
-        urls = ['/api/auth/login/', '/auth/token/logout/']
-        for url in urls:
-            try:
-                response = client.get(url)
-            except Exception as e:
-                assert False, f'''Страница `{url}` работает неправильно. Ошибка: `{e}`'''
-            assert response.status_code != 404, f'Страница `{url}` не найдена, проверьте этот адрес в *urls.py*'
-            assert response.status_code == 200, (
-                f'Ошибка {response.status_code} при открытиии `{url}`. Проверьте ее view-функцию'
-            )
 
     @pytest.mark.django_db(transaction=True)
     def test_users_not_authenticated(self, client):
