f74fe83a951b7be16cd10e061a32480a521f5aae
EvgVol <volochek93@yandex.ru>
2023-02-16 14:10:21 +0300

    update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,17 +1,16 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import UsersViewSet
+from .views import UsersViewSet, UserView
 
 app_name = 'users'
 
 
 router = routers.DefaultRouter()
-
 router.register(r'users', UsersViewSet, basename='users')
 
 urlpatterns = [
-    path('', incluse(router.urls)),
+    path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
     path('users/me/', UserView.as_view(), name='me'),
 ]
