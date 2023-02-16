2370f910bace5476f60fb444be26cf5423d8467b
EvgVol <volochek93@yandex.ru>
2023-02-16 14:08:22 +0300

    update setting

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('users/', include('users.urls')),
+    path('api/', include('users.urls')),
 ]
