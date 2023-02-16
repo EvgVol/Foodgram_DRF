237514f11b0e6a47998137f26feeceb937e7651f
EvgVol <volochek93@yandex.ru>
2023-02-16 11:38:27 +0300

    add admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Админка Пользователей."""
+
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+
+    list_filter = ('username', 'email',)
+    search_fields = ('username',)
+
