3e7be0780899412d505c9be85ae9d5001d8b5228
EvgVol <volochek93@yandex.ru>
2023-02-16 10:13:52 +0300

    add model.py in users

---
+++ backend/users/urls.py
@@ -0,0 +1,5 @@
+from django.urls import include, path
+from rest_framework import routers
+
+app_name = 'users'
+
---
+++ backend/users/validators.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+from django.conf import settings
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.exceptions import ValidationError
+
+
+USERNAME_REGEX = r'[\w\.@+-]+'
+NOT_ALLOWED_ME = ('Нельзя создать пользователя с '
+                  'именем: << {username} >> - это имя запрещено!')
+NOT_ALLOWED_CHAR_MSG = ('{chars} недопустимые символы '
+                        'в имени пользователя {username}.')
+
+
+def validate_username(username):
+    invalid_symbols = ''.join(set(re.sub(USERNAME_REGEX, '', username)))
+    if invalid_symbols:
+        raise ValidationError(
+            NOT_ALLOWED_CHAR_MSG.format(
+                chars=invalid_symbols, username=username))
+    if username == 'me':
+        raise ValidationError(NOT_ALLOWED_ME.format(username=username))
+    return username
+
+
+def username_me(value): 
+    """Проверка имени пользователя (me недопустимое имя).""" 
+    if value == 'me': 
+        raise ValidationError( 
+            'Имя пользователя "me" не разрешено.' 
+        ) 
+    return value 
+
+ 
+def validate_year(value): 
+    if value >= datetime.now().year: 
+        raise ValidationError( 
+            message=f'Год {value} больше текущего!', 
+            params={'value': value}, 
+        )
