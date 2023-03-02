b59898c131fedfd9bd4db5b4d340296cf10a1c76
EvgVol <volochek93@yandex.ru>
2023-03-02 22:04:29 +0300

    исправлены замечания по ревью

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import (Favorite, Ingredient, Recipe,
-                     IngredientInRecipe, ShoppingCart, Tag,)
+                     IngredientInRecipe, ShoppingCart, Tag)
 
 
 @admin.register(Ingredient)
@@ -20,21 +20,23 @@ class TagAdmin(admin.ModelAdmin):
     ordering = ('color',)
 
 
-class IngredientInRecipeAdmin(admin.TabularInline):
+
+class IngredientInRecipeInline(admin.TabularInline):
     model = IngredientInRecipe
     extra = 2
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'pub_date', 'name', 'text', 'cooking_time', 'get_tags', 'get_ingredients', 'count_favorites',)
+    list_display = ('id', 'pub_date', 'name', 'text', 'cooking_time',
+                    'get_tags', 'get_ingredients', 'count_favorites',)
     readonly_fields = ('count_favorites',)
     list_filter = ('name', 'tags',)
     search_fields = (
         'name', 'cooking_time',
         'author__email', 'ingredient__name')
     empty_value_display = '-пусто-'
-    inlines = (IngredientInRecipeAdmin,)
+    inlines = (IngredientInRecipeInline,)
 
     @admin.display(description='Количество в избранных')
     def count_favorites(self, obj):
@@ -47,15 +49,14 @@ class RecipeAdmin(admin.ModelAdmin):
         return '\n '.join([
             f'{item["ingredient__name"]} - {item["amount"]}'
             f' {item["ingredient__measurement_unit"]}.'
-            for item in obj.recipe.values(
+            for item in obj.ingredient_list.values(
                 'ingredient__name',
                 'amount', 'ingredient__measurement_unit')])
 
     @admin.display(description='Тэги')
     def get_tags(self, obj):
         """Получаем теги."""
-        list_ = [_.name for _ in obj.tags.all()]
-        return ', '.join(list_)
+        return ', '.join(_.name for _ in obj.tags.all())
 
 
 @admin.register(Favorite)
