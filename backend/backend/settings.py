76353b9132dcaf6f2c5f5f9f01f846928aef0282
EvgVol <volochek93@yandex.ru>
2023-03-07 08:37:18 +0300

    исправлено

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,9 @@ ALLOWED_HOSTS = {
 }
 
 CSRF_TRUSTED_ORIGINS = {
-    'CSRF_TRUSTED_ORIGINS': os.getenv('CSRF_TRUSTED_ORIGINS'),
+    'CSRF_TRUSTED_ORIGINS': os.getenv(
+        'CSRF_TRUSTED_ORIGINS', default='http://localhost',
+    ),
 }
 
 INSTALLED_APPS = [
