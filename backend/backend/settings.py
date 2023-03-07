906803f657a2fc33d659b0c0765e3d258e128c06
EvgVol <volochek93@yandex.ru>
2023-03-07 06:42:59 +0300

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,9 +13,9 @@ SECRET_KEY = {
 
 DEBUG = os.getenv('DEBUG', default=False) == 'True'
 
-ALLOWED_HOSTS = [
-    {'ALLOWED_HOSTS': os.getenv('ALLOWED_HOSTS'), }
-]
+ALLOWED_HOSTS = {
+    'ALLOWED_HOSTS': os.getenv('ALLOWED_HOSTS'),
+}
 
 CSRF_TRUSTED_ORIGINS = {
     'CSRF_TRUSTED_ORIGINS': os.getenv('CSRF_TRUSTED_ORIGINS'),
