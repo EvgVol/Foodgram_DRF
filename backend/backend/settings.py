1d6523399cf5002b256335f0e9d324ad17c2f586
EvgVol <volochek93@yandex.ru>
2023-03-07 21:44:26 +0300

    new deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,7 @@ DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ["84.201.163.40"]
 
-CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS')]
+CSRF_TRUSTED_ORIGINS = ["http://84.201.163.40"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
