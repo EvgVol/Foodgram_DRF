16a1025235f6387fbac46de47490651872326dd5
EvgVol <volochek93@yandex.ru>
2023-03-07 07:17:10 +0300

    update setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,13 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default="django.db.backends.postgresql"),
