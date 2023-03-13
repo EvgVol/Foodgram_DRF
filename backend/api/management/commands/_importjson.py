8bb9187e58ccab334ede66777c0171c73a89a9f1
EvgVol <volochek93@yandex.ru>
2023-03-13 13:56:48 +0300

    flake8

--- backend/api/management/commands/_importjson.py
+++ backend/api/management/commands/_importjson.py
@@ -19,10 +19,10 @@ def import_json():
             Ingredient.objects.get_or_create(**ingredients)
         print(f'Файл {data_file_ingredients.name} загружен.')
 
-    # with open(
-    #     os.path.join(FILE_DIR, 'tags.json'), encoding='utf-8'
-    # ) as data_file_tags:
-    #     tag_data = json.loads(data_file_tags.read())
-    #     for tags in tag_data:
-    #         Tag.objects.get_or_create(**tags)
-    #     print(f'Файл {data_file_tags.name} загружен.')
+    with open(
+        os.path.join(FILE_DIR, 'tags.json'), encoding='utf-8'
+    ) as data_file_tags:
+        tag_data = json.loads(data_file_tags.read())
+        for tags in tag_data:
+            Tag.objects.get_or_create(**tags)
+        print(f'Файл {data_file_tags.name} загружен.')
