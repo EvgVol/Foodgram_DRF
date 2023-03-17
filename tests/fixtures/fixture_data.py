25995707d68acd0d6f86ef437ad1345246cb16b5
Evgeniy V <volochek93@yandex.ru>
2023-03-17 16:21:32 +0400

    добавлены тесты для рецепттов

--- tests/fixtures/fixture_data.py
+++ tests/fixtures/fixture_data.py
@@ -1,6 +1,7 @@
 import pytest
+import json
 
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient, Tag, Recipe
 
 
 @pytest.fixture
@@ -43,3 +44,40 @@ def ingredient_3():
     return Ingredient.objects.create(
         name='Колбаса', measurement_unit='г'
     )
+
+
+# @pytest.fixture
+# def recipe_egg():
+#     return Recipe.objects.create(
+#         ingredients=[{"id": ingredient_1.id, "amount": 20},
+#                        {"id": ingredient_2.id, "amount": 2},
+#                        {"id": ingredient_3.id, "amount": 50}],
+#         tags=[tag_breakfast.id, tag_dinner.id],
+#         image=('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA'
+#                   'EAAAABAgMAAABieywaAAAACVBMVEUAAAD///9fX1/S0ecCAA'
+#                   'AACXBIWXMAAA7EAAAOxAGVKw4bAAAACklEQVQImWNoAAAAgg'
+#                   'CByxOyYQAAAABJRU5ErkJggg=='),
+#         name="Яичница с колбаской",
+#         text=("Берем сковородку наливаем масло. Жарим колбасу до золотистой "
+#               "корочки. Разбиваем яйца. Жарим ещё 3 минуты. Подаем. "
+#               "Приятного аппетита."),
+#         cooking_time=10
+#     )
+
+
+@pytest.fixture
+def new_recipe():
+    recipe = {
+        "ingredients": [json.dumps({"id": 1, "amount": 10}), ],
+        "tags": [1, 2],
+        "image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAgMAAABieywaAAAACVBMVEUAAAD///9fX1/S0ecCAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAACklEQVQImWNoAAAAggCByxOyYQAAAABJRU5ErkJggg==",  # noqa
+        "name": "string",
+        "text": "string",
+        "cooking_time": 1
+    }
+    return recipe
+
+
+@pytest.fixture
+def recipes():
+    return ('/api/recipes/')
---
+++ tests/test_recipes.py
@@ -0,0 +1,51 @@
+import pytest
+from recipes.models import Recipe
+
+
+class Test07RecipeAPI:
+
+    @pytest.mark.django_db(transaction=True)
+    def test_01_recipe_get_users(self, client, auth_client_1, recipes, new_recipe):
+        response  = client.get(recipes)
+        assert response.status_code == 200, (
+            f'Проверьте, что при GET запросе страница `{recipes}` '
+            'возвращает статус 200'
+        )
+        response  = auth_client_1.get(recipes)
+        assert response.status_code == 200, (
+            f'Проверьте, что при GET запросе страница `{recipes}` '
+            'возвращает статус 200'
+        )
+        recipe_count = Recipe.objects.count()
+        assert response.json().get('count') == recipe_count, (
+            f'Проверьте, что при GET запросе страница `{recipes}` '
+            'возвращается правильное количество рецептов'
+        )
+        data = response.json()
+        assert 'count' in data, (
+            f'Проверьте, что при GET запросе `{recipes}` возвращаете данные'
+             ' с пагинацией. Не найден параметр `count`'
+        )
+        assert 'next' in data, (
+            f'Проверьте, что при GET запросе `{recipes}` возвращаете данные'
+             ' с пагинацией. Не найден параметр `next`'
+        )
+        assert 'previous' in data, (
+            f'Проверьте, что при GET запросе `{recipes}` возвращаете данные'
+             ' с пагинацией. Не найден параметр `previous`'
+        )
+        assert 'results' in data, (
+            f'Проверьте, что при GET запросе `{recipes}` возвращаете данные'
+             ' с пагинацией. Не найден параметр `results`'
+        )
+        assert type(data['results']) == list, (
+            f'Проверьте, что при GET запросе `{recipes}` возвращаете данные'
+             ' с пагинацией. Тип параметра `results` должен быть список'
+        )
+        assert (
+            len(data['results']) == recipe_count
+        ), (
+            f'Проверьте, что при GET запросе `{recipes}` '
+             'возвращаете данные с пагинацией. Значение параметра `results` '
+             'не правильное'
+        )
